fd5b248ccb1be77750b42ded10ae56805daa1aab
strawberrybiscuits <441664249@qq.com>
2023-04-10 14:15:36 +0800

    Update PackerFuzzer.py
    支持多url扫描，文件列表

--- PackerFuzzer.py
+++ PackerFuzzer.py
@@ -6,7 +6,8 @@ from lib.TestProxy import testProxy
 from lib.common.banner import RandomBanner
 from lib.common.cmdline import CommandLines
 from lib.common.readConfig import ReadConfig
-
+from lib.common.FileRead import FileRead
+from concurrent.futures import ThreadPoolExecutor,wait, ALL_COMPLETED
 
 class Program():
     def __init__(self,options):
@@ -14,9 +15,24 @@ class Program():
 
     def check(self):
         url = self.options.url
-        t = Project(url,self.options)
-        t.parseStart()
-
+        if url != None:
+            t = Project(url,self.options)
+            t.parseStart()
+        else :
+            #不能放循环，循环内多线程无法控制。循环结束漏洞无法分析、报告无法生成
+            file = self.options.file
+            urlLists = FileRead(file).getUrlLists()
+            pool = ThreadPoolExecutor(10)
+            allTask = [pool.submit(self.worker, domain) for domain in urlLists]
+            wait(allTask, return_when=ALL_COMPLETED)
+                
+    def worker(self,url):
+        try:
+            t = Project(url,self.options)
+            t.parseStart()
+        except Exception as e:
+            print("error :%s" % e)
+            
 
 if __name__ == '__main__':
     cmd = CommandLines().cmd()
